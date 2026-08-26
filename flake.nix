cf18ad5ade855233bd70bb48d40d82d25a9e0a3d
Kieran Hannigan <overyonderstudios@gmail.com>
2026-08-27 09:26:16 +1000

    Add reproducible Rust development shell

--- flake.nix
+++ flake.nix
@@ -3,17 +3,26 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     self,
     nixpkgs,
+    rust-overlay,
   }: let
     systems = ["x86_64-linux" "aarch64-linux"];
     forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    pkgsFor = system:
+      import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
   in {
     packages = forAllSystems (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = pkgsFor system;
       rstat = pkgs.rustPlatform.buildRustPackage {
         pname = "rstat";
         version = "0.1.0";
@@ -26,6 +35,28 @@
     in {
       default = rstat;
       rstat = rstat;
+    });
+
+    devShells = forAllSystems (system: let
+      pkgs = pkgsFor system;
+      rustToolchain = pkgs.rust-bin.stable."1.96.0".default.override {
+        extensions = ["clippy" "rust-analyzer" "rust-src" "rustfmt"];
+      };
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          rustToolchain
+          bpftools
+          clang
+          libbpf
+          mold
+          pkg-config
+        ];
+        RSTAT_BPFTOOL = "${pkgs.bpftools}/bin/bpftool";
+        RSTAT_CLANG = "${pkgs.llvmPackages.clang-unwrapped}/bin/clang";
+        RSTAT_LIBBPF_INCLUDE = "${pkgs.libbpf}/include";
+        RUSTFLAGS = "-C link-arg=-fuse-ld=mold";
+      };
     });
   };
 }
